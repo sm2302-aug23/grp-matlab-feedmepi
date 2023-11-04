e5982e52fa70abc8b715657402bfc50509b626b6
AqilMN <19b9002@ubd.edu.bn>
2023-11-04 17:59:30 +0800

    Update names.m
    added cursive for aqil

--- names.m
+++ names.m
@@ -21,3 +21,28 @@ xlabel('x')
 ylabel('y')
 set(gca, 'FontSize', 10, 'LineWidth', 1)
 grid on
+
+% cursive name of second member - aqil
+
+x = [3.4 2 1.2 1.9 3.2 3.4 3.2 3.7 4.9 5.6 6.8 6.7 6.6 5.6 4.9 5.6 6.8 6.7 6.6 5.6 4.9 5.6 6.8 6.7 6.6 6.4 6.1 5.7 6 6.5 6.4 7 8 8.5 9 9.4 9.1 9.1 9.8 10.5 11.65 12.2 12.5 11.95 11.65 11.5 12 13];
+y = [4 3.7 2.8 2 3 4 3 2 2.7 3.8 4 3.35 2.7 2 2.7 3.8 4 3.35 2.7 2 2.7 3.8 4 3.35 2.7 2.2 1.5 0.6 0 0.6 2.2 2.35 2 2.2 3 3.9 3 2.2 2 2.3 3.8 4.7 5.7 4.7 3.8 2.4 2 2.5];
+
+n = length(x);
+t = 0:n-1;
+tt = 0:0.01:n-1;
+
+xx = spline(t, x, tt);
+yy = spline(t, y, tt);
+
+figure(2)
+plot(xx, yy, 'k', 'LineWidth', 1.5)
+hold on
+plot(x, y , 'bo', 'MarkerFaceColor', 'b')
+grid on
+xlabel('x')
+ylabel('y')
+set(gca, 'FontSize', 10, 'LineWidth', 1)
+grid on
+pos = [9.5 4.4 0.25 0.25];
+rectangle('Position',pos, 'Curvature',[1 1])
+axis equal
